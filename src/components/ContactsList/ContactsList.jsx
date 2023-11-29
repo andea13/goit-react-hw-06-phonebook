031d387e2b78e5118981fd4a298424786144d6f0
Oksana Dziuba <dziuba131303@gmail.com>
2023-11-29 23:56:02 +0400

    deletes extra logs

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -2,16 +2,10 @@ import { ContactsListItem } from '../ContactsListItem/ContactsListItem';
 import { ContactList } from './ContactsList.styled';
 import { useSelector } from 'react-redux';
 import { getContacts, getFilter } from 'redux/ContactsSlice';
-// import { useMemo } from 'react';
-
-// import { useMemo } from 'react';
 
 export const ContactsList = () => {
   const { contacts } = useSelector(getContacts);
   const { filter } = useSelector(getFilter);
-
-  console.log(contacts);
-  console.log(filter);
 
   return (
     <ContactList>
--- src/redux/ContactsSlice.js
+++ src/redux/ContactsSlice.js
@@ -1,64 +1,3 @@
-// import { createSlice } from '@reduxjs/toolkit';
-// import { persistReducer } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
-
-// // let contact = [
-// //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-// //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-// //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-// //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-// // ];
-
-// const initialState = {
-//   contacts: [],
-//   filter: '',
-// };
-
-// console.log(initialState);
-
-// export const ContactsSlice = createSlice({
-//   name: 'contacts',
-//   initialState,
-//   reducers: {
-//     addContact: (state, action) => {
-//       const duplicate = state.contacts.find(
-//         item => item.name.toLowerCase() === action.payload.name.toLowerCase()
-//       );
-//       if (duplicate) {
-//         alert(`${action.payload.name} is already in contacts`);
-//         return;
-//       }
-//       state.contacts.push(action.payload);
-//     },
-
-//     setFilter: (state, action) => {
-//       console.log(action.payload);
-//       state.filter = action.payload;
-//     },
-
-//     deleteContact: (state, action) => {
-//       state.contacts = state.contacts.filter(
-//         contact => contact.id !== action.payload
-//       );
-//     },
-//   },
-// });
-
-// const persistConfig = {
-//   key: 'root',
-//   storage,
-//   whitelist: ['contacts'],
-// };
-
-// export const ContactsReducer = persistReducer(
-//   persistConfig,
-//   ContactsSlice.reducer
-// );
-
-// export const getContactsAndFilterValue = state => state.contacts;
-
-// export const { addContact, setFilter, deleteContact } = ContactsSlice.actions;
-
 import { createSlice } from '@reduxjs/toolkit';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
@@ -100,7 +39,6 @@ export const ContactsSlice = createSlice({
 const persistConfig = {
   key: 'contacts',
   storage,
-  // whitelist: ['contacts'],
 };
 
 export const ContactsReducer = persistReducer(
